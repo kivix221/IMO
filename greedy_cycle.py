c7e3fdecf05e22f5955bb06a05cd239bb1b0c506
Eryk Szpotański <eryk.szpotanski@student.put.poznan.pl>
2021-03-05 15:11:36 +0100

    greedy cycle algorithm

---
+++ greedy_cycle.py
@@ -0,0 +1,78 @@
+import numpy as np
+import sys
+
+
+def get_first_node(matrix: np.ndarray, not_been: list, node=0) -> int:
+    i, m = -1, sys.maxsize
+    for v in not_been:
+        if matrix[v, node] < m:
+            m = matrix[v, node]
+            i = v
+    # i = np.argmin(np.delete(matrix[node], [node]))
+    # if i >= node:
+    #     i += 1
+    return i
+
+
+def get_cycle_next_node(matrix: np.ndarray, not_been: list, cycle: list) -> (int, int):
+    mi, mv = 0, sys.maxsize
+    ci = -1
+    for ix, vx in enumerate(not_been):
+        for i in range(len(cycle) - 1):
+            dis = matrix[vx, cycle[i]] + matrix[cycle[i + 1], vx]
+            if dis < mv:
+                ci = i
+                mi = ix
+                mv = dis
+        dis = matrix[vx, cycle[0]] + matrix[cycle[len(cycle) - 1], vx]
+        if dis < mv:
+            ci = len(cycle) - 1
+            mi = ix
+            mv = dis
+    return mi, ci
+
+
+def basic_greedy_cycle(matrix: np.ndarray, node=0) -> list:
+    not_been = list(range(len(matrix)))
+    not_been.pop(node)
+    cycle = [node, get_first_node(matrix, not_been, node)]
+    not_been.remove(cycle[-1])
+
+    while len(not_been) != 0:
+        inx, ci = get_cycle_next_node(matrix, not_been, cycle)
+        cycle.insert(ci + 1, not_been[inx])
+        not_been.pop(inx)
+
+    return cycle
+
+
+def double_greedy_cycle(matrix: np.ndarray, node=0) -> (list, list):
+    matrix[node, node] = 0
+    # print(matrix[node])
+    sec_node = np.argmax(matrix[node])
+    matrix[node, node] = np.inf
+    # print(sec_node)
+    
+    not_been = list(range(len(matrix)))
+    not_been.pop(node)
+    # print(not_been)
+    not_been.remove(sec_node)
+    cycle1 = [node, get_first_node(matrix, not_been, node)]
+    not_been.remove(cycle1[-1])
+    cycle2 = [sec_node, get_first_node(matrix, not_been, sec_node)]
+    not_been.remove(cycle2[-1])
+
+    while len(not_been) != 0:
+        inx, ci = get_cycle_next_node(matrix, not_been, cycle1)
+        cycle1.insert(ci + 1, not_been[inx])
+        not_been.pop(inx)
+
+        if len(not_been) == 0:
+            break
+        inx, ci = get_cycle_next_node(matrix, not_been, cycle2)
+        cycle2.insert(ci + 1, not_been[inx])
+        not_been.pop(inx)
+
+    cycle1.append(node)
+    cycle2.append(sec_node)
+    return cycle1, cycle2
--- tsp.py
+++ tsp.py
@@ -1,5 +1,6 @@
-from utils import load_instance,calc_distance_matrix, plot_result
+from utils import load_instance, calc_distance_matrix, plot_result
 from nn import nn_alg
+from greedy_cycle import *
 
 ##########
 kroa100_instance = load_instance('kroa100.tsp')
@@ -8,14 +9,19 @@ krob100_instance = load_instance('krob100.tsp')
 kroa100_distance_matrix = calc_distance_matrix(kroa100_instance)
 krob100_distance_matrix = calc_distance_matrix(krob100_instance)
 
-
-first_cycle_kroa100, second_cycle_kroa100 = nn_alg(kroa100_distance_matrix) 
+first_cycle_kroa100, second_cycle_kroa100 = nn_alg(kroa100_distance_matrix)
 first_cycle_krob100, second_cycle_krob100 = nn_alg(krob100_distance_matrix)
 
 # print(first_cycle_kroa100)
 # print(second_cycle_kroa100)
 
-plot_result(kroa100_instance, first_cycle_kroa100,second_cycle_kroa100)
-plot_result(krob100_instance, first_cycle_krob100,second_cycle_krob100)
+plot_result(kroa100_instance, first_cycle_kroa100, second_cycle_kroa100)
+plot_result(krob100_instance, first_cycle_krob100, second_cycle_krob100)
 
+#    Greedy cycle
 
+first_cycle_kroa100, second_cycle_kroa100 = double_greedy_cycle(kroa100_distance_matrix)
+first_cycle_krob100, second_cycle_krob100 = double_greedy_cycle(krob100_distance_matrix)
+
+plot_result(kroa100_instance, first_cycle_kroa100, second_cycle_kroa100)
+plot_result(krob100_instance, first_cycle_krob100, second_cycle_krob100)
