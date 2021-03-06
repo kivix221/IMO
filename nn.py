089a2f59b06628ac81e5af5c3df902bccb59bb75
kivi221 <kivi9831@gmail.com>
2021-03-06 15:44:24 +0100

    fix nn (2)

--- nn.py
+++ nn.py
@@ -1,34 +1,68 @@
 import numpy as np
 import random
+from utils import get_cycle_distance
 
-def nn_alg(distance_matrix, node=0):
-    first_cycle =  [random.randint(0,99)]
-    second_cycle = [random.randint(0,99)]
 
-    while len(first_cycle) < len(distance_matrix)/2:
-        first_cycle_city = nearest_city(first_cycle + second_cycle, distance_matrix, first_cycle[len(first_cycle) - 1]) 
-        first_cycle.append(first_cycle_city) 
-     
-        second_cycle_city = nearest_city(first_cycle + second_cycle, distance_matrix, second_cycle[len(second_cycle) - 1]) 
-        if second_cycle_city != -1:
-            second_cycle.append(second_cycle_city)
+def nn_alg(distance_matrix, node = 0):
+    not_been = list(range(len(distance_matrix)))
 
-    first_cycle.append(first_cycle[0])
-    second_cycle.append(second_cycle[0])
-
-    return first_cycle, second_cycle
-
-def nearest_city(used_cities,dist_matrix, current_city):
-    max_dist = np.inf
-    nearest_city = -1
+   
+    cycle1_current_node = not_been.pop(random.randint(0,99))
+    #drugi losowo
+    cycle2_current_node = not_been.pop(random.randint(0,98))
     
-    for i in range(len(dist_matrix[current_city])):
-        if i in used_cities:
-            continue
+    #drugi jak najdalej
+    # farest_node = np.where(np.isinf(distance_matrix[cycle1_current_node]),
+    #             -np.inf,distance_matrix[cycle1_current_node]).argmax()
+    # cycle2_current_node = not_been.pop(not_been.index(farest_node))
+                 
+    cycle1 = [cycle1_current_node]
+    cycle2 = [cycle2_current_node]
 
-        if dist_matrix[current_city,i] < max_dist:
-            max_dist = dist_matrix[current_city,i]
-            nearest_city = i
+    while len(not_been) != 0:
+        cycle1_new_node = get_nearest_node(distance_matrix,not_been, cycle1_current_node)
+        cycle1_insert_index =  get_best_insertion(distance_matrix,cycle1,cycle1_new_node)
+        cycle1.insert(cycle1_insert_index,cycle1_new_node)
+        cycle1_current_node = cycle1_new_node
 
-    return nearest_city
+        cycle2_new_node = get_nearest_node(distance_matrix,not_been,cycle2_current_node)
+        cycle2_insert_index = get_best_insertion(distance_matrix,cycle2,cycle2_new_node)
+        cycle2.insert(cycle2_insert_index,cycle2_new_node)
+        cycle2_current_node = cycle2_new_node  
+
+    cycle1.append(cycle1[0])
+    cycle2.append(cycle2[0])
+
+    return cycle1, cycle2
+
+
+def get_nearest_node(distance_matrix,not_been, current_node):
+    min_dist = np.inf
+    nearest_node = None
+
+    for index_not_been, node_not_been in enumerate(not_been):
+       if distance_matrix[current_node, node_not_been] < min_dist:
+            min_dist = distance_matrix[current_node, node_not_been]
+            nearest_node = index_not_been
+
+    return not_been.pop(nearest_node)
+
+
+def get_best_insertion(distance_matrix,cycle,new_node):
+    if len(cycle) == 1:
+        return 1
+
+    min_total_distance = np.inf
+    best_insertion = None
+
+    for i in range(len(cycle) + 1):
+        cycle.insert(i,new_node)
+        total_distance = get_cycle_distance(distance_matrix,cycle)
+        if(total_distance< min_total_distance):
+            min_total_distance = total_distance
+            best_insertion = i
+        cycle.pop(i)
+
+    return best_insertion
+
 
--- tsp.py
+++ tsp.py
@@ -1,7 +1,7 @@
 from utils import load_instance, calc_distance_matrix, plot_result, get_cycles_distance
 from nn import nn_alg
 from greedy_cycle import *
-
+ 
 ##########
 kroa100_instance = load_instance('kroa100.tsp')
 krob100_instance = load_instance('krob100.tsp')
@@ -9,11 +9,10 @@ krob100_instance = load_instance('krob100.tsp')
 kroa100_distance_matrix = calc_distance_matrix(kroa100_instance)
 krob100_distance_matrix = calc_distance_matrix(krob100_instance)
 
+# nn
 first_cycle_kroa100, second_cycle_kroa100 = nn_alg(kroa100_distance_matrix)
 first_cycle_krob100, second_cycle_krob100 = nn_alg(krob100_distance_matrix)
 
-# print(first_cycle_kroa100)
-# print(second_cycle_kroa100)
 
 print(get_cycles_distance(kroa100_distance_matrix, first_cycle_kroa100, second_cycle_kroa100))
 print(get_cycles_distance(krob100_distance_matrix, first_cycle_krob100, second_cycle_krob100))
@@ -21,18 +20,18 @@ print(get_cycles_distance(krob100_distance_matrix, first_cycle_krob100, second_c
 plot_result(kroa100_instance, first_cycle_kroa100, second_cycle_kroa100)
 plot_result(krob100_instance, first_cycle_krob100, second_cycle_krob100)
 
-#    Greedy cycle
+# Greedy cycle
 
-# first_cycle_kroa100, second_cycle_kroa100 = double_greedy_cycle(kroa100_distance_matrix)
-# first_cycle_krob100, second_cycle_krob100 = double_greedy_cycle(krob100_distance_matrix)
-#
-# plot_result(kroa100_instance, first_cycle_kroa100, second_cycle_kroa100)
-# plot_result(krob100_instance, first_cycle_krob100, second_cycle_krob100)
-#
-# print(get_cycles_distance(kroa100_distance_matrix, first_cycle_kroa100, second_cycle_kroa100))
-# print(get_cycles_distance(krob100_distance_matrix, first_cycle_krob100, second_cycle_krob100))
+first_cycle_kroa100, second_cycle_kroa100 = double_greedy_cycle(kroa100_distance_matrix)
+first_cycle_krob100, second_cycle_krob100 = double_greedy_cycle(krob100_distance_matrix)
 
-#    Greedy cycle seq
+plot_result(kroa100_instance, first_cycle_kroa100, second_cycle_kroa100)
+plot_result(krob100_instance, first_cycle_krob100, second_cycle_krob100)
+
+print(get_cycles_distance(kroa100_distance_matrix, first_cycle_kroa100, second_cycle_kroa100))
+print(get_cycles_distance(krob100_distance_matrix, first_cycle_krob100, second_cycle_krob100))
+
+#Greedy cycle seq
 
 first_cycle_kroa100, second_cycle_kroa100 = double_greedy_cycle_seq(kroa100_distance_matrix)
 first_cycle_krob100, second_cycle_krob100 = double_greedy_cycle_seq(krob100_distance_matrix)
@@ -43,7 +42,7 @@ plot_result(krob100_instance, first_cycle_krob100, second_cycle_krob100)
 print(get_cycles_distance(kroa100_distance_matrix, first_cycle_kroa100, second_cycle_kroa100))
 print(get_cycles_distance(krob100_distance_matrix, first_cycle_krob100, second_cycle_krob100))
 
-#    Regret
+#Regret
 
 first_cycle_kroa100, second_cycle_kroa100 = double_regret_cycle2(kroa100_distance_matrix)
 first_cycle_krob100, second_cycle_krob100 = double_regret_cycle2(krob100_distance_matrix)
