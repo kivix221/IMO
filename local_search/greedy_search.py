148a024590e53128855731e97d05cad76a9ae0fb
kivi221 <kivi9831@gmail.com>
2021-03-22 21:17:02 +0100

    Added greedy search, modified generate random cycles

--- local_search/greedy_search.py
+++ local_search/greedy_search.py
@@ -1,7 +1,6 @@
 from typing import List, Optional
-
 import numpy as np
-from ..utils import get_random_cycle, load_instance, get_cycles_distance, calc_distance_matrix
+from ..utils import get_random_cycle, load_instance, get_cycles_distance, calc_distance_matrix,plot_result
 import time
 from random import randrange, choice
 
@@ -43,6 +42,7 @@ def greedy_swap_vertices_in_cycle(matrix: np.ndarray, cycle1: np.ndarray, cycle2
 def search_swap_vertices_between_cycle(matrix, cycle1, cycle2) -> (Optional[int], Optional[int]):
     size1, size2 = len(cycle1), len(cycle2)
     it1, it2 = np.arange(size1), np.arange(size2)
+   
     np.random.shuffle(it1)
     np.random.shuffle(it2)
     if choice([True, False]):
@@ -65,7 +65,6 @@ def search_swap_vertices_between_cycle(matrix, cycle1, cycle2) -> (Optional[int]
                     return first, second
     return None, None
 
-
 def greedy_swap_vertices_between_cycle(matrix: np.ndarray, cycle1: np.ndarray, cycle2: np.ndarray) -> (
         List[int], List[int]):
     search = True
@@ -77,27 +76,85 @@ def greedy_swap_vertices_between_cycle(matrix: np.ndarray, cycle1: np.ndarray, c
             cycle1[f], cycle2[s] = cycle2[s], cycle1[f]
     return cycle1, cycle2
 
+#1)z.w + 2)z.k
+def greedy_swap_edges_in_cycle(matrix,cycle1,cycle2):
+    search = True
+    search_between_cycles = True
+    search_in_cycle = True
+    while search:
+        # Randomizuje typ ruchu czyli albo pomiędzy albo w wewnątrz cyklu
+        if choice([True, False]):
+            f, s = search_swap_vertices_between_cycle(matrix, cycle1, cycle2)
+            if f is None or s is None:
+                search_between_cycles = False
+            else:
+                search_between_cycles = True
+                cycle1[f], cycle2[s] = cycle2[s], cycle1[f]
+        else:
+            #randomizacja cykli -> ruch wewnątrz pierwszego lub drugiego
+            if choice([True,False]):
+                i,j = search_swap_edges_in_cycle_greedy(matrix,cycle1)
+                if i is None or j is None:
+                    search_in_cycle = False
+                else:
+                    search_in_cycle = True
+                    cycle1[i+1], cycle1[j] = cycle1[j], cycle1[i+1]
+                    cycle1[i + 2:j] = cycle1[i + 2:j][::-1]
+            else:
+                i,j = search_swap_edges_in_cycle_greedy(matrix,cycle2)
+                if i is None or j is None:
+                    search_in_cycle = False
+                else:
+                    search_in_cycle = True
+                    cycle2[i+1], cycle2[j] = cycle2[j], cycle2[i+1]
+                    cycle2[i + 2:j] = cycle2[i + 2:j][::-1]
+        
+        search = search_between_cycles or search_in_cycle
 
-def greedy_swap_edges_in_cycle(matrix, cycle1, cycle2):
-    pass
+    return cycle1,cycle2
 
-
-def greedy_swap_edges_between_cycle(matrix, cycle1, cycle2):
-    pass
-
-
+def search_swap_edges_in_cycle_greedy(matrix,cycle):
+    for i in range(len(cycle) - 1):
+        for j in range(i +2, len(cycle) - 1):
+            old_distance = matrix[cycle[i]][cycle[i+1]] + matrix[cycle[j]][cycle[j+1]]
+            new_distance = matrix[cycle[i]][cycle[j]] + matrix[cycle[i+1]][cycle[j+1]]
+        
+            if new_distance - old_distance < 0:
+               return i,j
+    return None, None
+               
+                   
 if __name__ == "__main__":
     kroa100_instance = load_instance('data/kroa100.tsp')
     kroa100_distance_matrix = calc_distance_matrix(kroa100_instance)
 
     k_cycle1, k_cycle2 = get_random_cycle()
-    v_cycle1, v_cycle2 = k_cycle1.copy(), k_cycle2.copy()
-    print(get_cycles_distance(kroa100_distance_matrix, k_cycle1, k_cycle2))
+    print("Pierwotnie: ", get_cycles_distance(kroa100_distance_matrix, k_cycle1, k_cycle2))
+    plot_result(kroa100_instance,k_cycle1,k_cycle2)
+    
+    v_cycle1, v_cycle2 = k_cycle1.copy(), k_cycle2.copy() #do greedy swap vert in cycle
+    v2_cycle1, v2_cycle2 = k_cycle1.copy(), k_cycle2.copy() #do greedy swap vert beetween cycle
+    v3_cycle1, v3_cycle2 = k_cycle1.copy(), k_cycle2.copy() #do greedy swap edges
 
-    v_cycle1, v_cycle2 = greedy_swap_vertices_in_cycle(kroa100_distance_matrix, v_cycle1, v_cycle2)
+    #swap vertices
     duration = time.time()
-    k_cycle1, k_cycle2 = greedy_swap_vertices_between_cycle(kroa100_distance_matrix, k_cycle1, k_cycle2)
+    v_cycle1, v_cycle2 = greedy_swap_vertices_in_cycle(kroa100_distance_matrix, v_cycle1, v_cycle2)
     duration = time.time() - duration
-    print(get_cycles_distance(kroa100_distance_matrix, v_cycle1, v_cycle2))
-    print(get_cycles_distance(kroa100_distance_matrix, k_cycle1, k_cycle2))
-    print(duration)
+    print("Greedy swap vert in cycle: ",get_cycles_distance(kroa100_distance_matrix, v_cycle1, v_cycle2))
+    print("Greedy swap vert in cycle: ",duration)
+    plot_result(kroa100_instance,v_cycle1,v_cycle2)
+  
+    duration = time.time()
+    v2_cycle1, v2_cycle2 = greedy_swap_vertices_between_cycle(kroa100_distance_matrix, v2_cycle1, v2_cycle2)
+    duration = time.time() - duration
+    print("Greedy swap vert between cycles: ",get_cycles_distance(kroa100_distance_matrix, v2_cycle1, v2_cycle2))
+    print("Greedy swap vert between cycles: ",duration)
+    plot_result(kroa100_instance,v2_cycle1,v2_cycle2)
+  
+    #swap edges
+    duration = time.time()
+    v3_cycle1, v3_cycle2 = greedy_swap_edges_in_cycle(kroa100_distance_matrix,v3_cycle1,v3_cycle2)
+    duration = time.time() - duration
+    print("Greedy swap edges: ",get_cycles_distance(kroa100_distance_matrix, v3_cycle1, v3_cycle2))
+    print("Greedy swap edges: ",duration)
+    plot_result(kroa100_instance,v3_cycle1,v3_cycle2)
