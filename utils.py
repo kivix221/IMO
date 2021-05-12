f1a885921a89d1d7027a551c7af2cad6a641cbf7
Eryk Szpotański <eryk.szpotanski@student.put.poznan.pl>
2021-05-12 14:33:39 +0200

    update

--- utils.py
+++ utils.py
@@ -36,13 +36,13 @@ def plot_result(instance, first_cycle, second_cycle,instance_title):
     first_cycle_to_plot = np.asfarray([instance[i] for i in first_cycle])
     second_cycle_to_plot = np.asfarray([instance[i] for i in second_cycle])
 
-    plt.figure()
-    plt.title(instance_title)
+    # plt.figure()
     plt.plot(first_cycle_to_plot[:, 0], first_cycle_to_plot[:, 1], color='red', label = 'Cykl 1')
     plt.plot(second_cycle_to_plot[:, 0], second_cycle_to_plot[:, 1], color='blue', label = 'Cykl 2')
     plt.scatter(instance[:, 0], instance[:, 1], color='black')
+    plt.title(instance_title)
     plt.legend()
-    # plt.show()
+    plt.show()
 
 
 def get_cycle_distance(matrix: np.ndarray, cycle: Iterable[int]) -> int:
